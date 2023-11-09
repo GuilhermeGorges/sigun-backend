f3bc473e1cf44498a05c5d8370a669eacc9fcf7c
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2023-11-09 19:59:41 -0300

    Remove all

--- models/ProfileType.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const profileTypeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-const ProfileType = mongoose.model('ProfileType', profileTypeSchema);
-
-module.exports = ProfileType;
